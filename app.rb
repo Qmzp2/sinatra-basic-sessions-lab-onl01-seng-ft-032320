d5218e23c0cf9b92b61fb09944c6287991ce874c
Kevin Mathias <komathias1@gmail.com>
2020-05-14 11:07:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,9 +1,19 @@
+
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  
-  get '/' do
-    
+  configure do
+    enable :sessions
+    set :session_secret, "secret" #the string can be whatever you want, like a 'password' I guess? So not secret, haha.
   end
-  
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    @sessions = session
+    item = params["item"]
+    @sessions[:item] = item
+  end
 end
