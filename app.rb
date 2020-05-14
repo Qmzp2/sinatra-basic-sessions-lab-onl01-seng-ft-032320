0671a4d3b1c154bf7f71eecebb35bab1ba315090
Kevin Mathias <komathias1@gmail.com>
2020-05-14 11:17:20 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class App < Sinatra::Base
 
   post '/checkout' do #don't quit follow whats going on here
     @sessions = session
-    params["item"]
-    @sessions[:item] = item
+    
+    @sessions[:item] = itemparams["item"]
   end
 end
